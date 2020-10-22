59965f4b54f8002de11bb53dd81f8afbb196480c
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-10-22 15:03:35 -0500

    Create spark-env.sh

---
+++ config/spark-env.sh
@@ -0,0 +1,2 @@
+SPARK_MASTER_HOST='IP' #local IP; should be the same IP used with Spark Master in spark-defaults.conf
+HADOOP_CONF_DIR='/s1/snagaraj/hadoop/etc/hadoop' #directory with hadoop configuration files
