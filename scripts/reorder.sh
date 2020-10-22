fa9c76a91ead2c8982c2709a42a3f72ff5a64b7a
snagaraj0 <40141880+snagaraj0@users.noreply.github.com>
2020-10-22 15:15:28 -0500

    Create reorder.sh

---
+++ scripts/reorder.sh
@@ -0,0 +1,2 @@
+#Shell script to reorder SAM file by ID number. Pass the file made by SparkMap, the delimiter that separates each part of the ID(i.e. colon, period, etc) and output file name.
+python reorder.py test.sam . test_ordered.sam
